adf6569d2ac69f019f38bced6eed901861969305
Nil Caream <nil.caream@gmail.com>
2023-03-04 20:44:17 +0100

    Added base paper throw action.

--- confetti.js
+++ confetti.js
@@ -2,6 +2,20 @@
 
     let timestamp = 0;
 
+    const papers = [];
+
+    class Paper {
+        constructor(x, y, v0, angle) {
+            this.x = x;
+            this.y = y;
+            this.vX = v0 * Math.cos(angle);
+            this.vY = v0 * Math.sin(angle);
+            this.x0 = x;
+            this.y0 = y;
+            this.t = 0;
+        }
+    }
+
     const mouse = {
         x0: 0,
         y0: 0,
@@ -36,6 +50,15 @@
 
         canvas.addEventListener("mouseup", () => {
             mouse.click = false;
+
+            const xD = mouse.x0 - mouse.x1;
+            const yD = mouse.y1 - mouse.y0;
+            const v0 = Math.sqrt(xD * xD + yD * yD);
+            const angle = Math.atan2(yD, xD);
+
+            for (let i = 0; i < 50; i++) {
+                papers.push(new Paper(mouse.x0, mouse.y0, v0, angle));
+            }
         });
 
         return canvas;
@@ -46,6 +69,22 @@
 
     const animate = diff => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        papers.forEach(paper => {
+            paper.t += diff;
+            paper.x = paper.x0 + paper.vX * paper.t * 0.01;
+            paper.y = paper.y0 - paper.vY * paper.t * 0.01;
+
+            ctx.save();
+            ctx.translate(paper.x, paper.y);
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(10, 0);
+            ctx.lineTo(10, 10);
+            ctx.lineTo(0, 10);
+            ctx.fill();
+            ctx.restore();
+        });
 
         if (mouse.click) {
             ctx.save();
